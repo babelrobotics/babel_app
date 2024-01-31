d306a525c57cbac0686d267acde3f38344020c78
Khoas1234567890 <yassinarsenal5@gmail.com>
2024-01-31 18:30:54 +0000

    design stuff

--- lib/profile/settings.dart
+++ lib/profile/settings.dart
@@ -9,9 +9,7 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SettingsPage(user: user),
-    );
+    return SettingsPage(user: user);
   }
 }
 
@@ -26,10 +24,12 @@ class SettingsPage extends StatelessWidget {
       SETTINGS_SECTION_DETAILS: {
         DETAILS_EMAIL: user.email,
         DETAILS_NAME: user.name,
+        "Free Trial": "7 Days Remaining",
       },
       SETTINGS_SECTION_PREFERENCES: {
-        "Theme": "light",
-        "PictureBorder": "square",
+        "Theme": "Light",
+        "PictureBorder": "Square",
+        "Notifications": "Deliver Quietly",
       },
     };
 
@@ -38,37 +38,44 @@ class SettingsPage extends StatelessWidget {
         title: Text('Account', style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.white,
         centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
-        ),
       ),
       body: Container(
-        color: Colors.grey,
+        color: Colors.grey[200],
         child: ListView(
-          children: mapSectionFieldValues.entries.map((section) => buildSection(section, context)).toList(),
+          children: mapSectionFieldValues.entries
+              .map((section) => buildSection(section, context))
+              .toList(),
         ),
       ),
     );
   }
 
-  Widget buildSection(MapEntry<String, Map<String, String>> section, BuildContext context) {
+  Widget buildSection(
+      MapEntry<String, Map<String, String>> section, BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(8.0), // Add margin for the section
+      margin: EdgeInsets.all(12.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              section.key,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+          if (section.key != SETTINGS_SECTION_DETAILS)
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                section.key,
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
             ),
-          ),
           Card(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)), // Rounded corners
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)), // Rounded corners
             child: Column(
-              children: section.value.entries.map((field) => buildListTile(field, context)).toList(),
+              children: section.value.entries
+                  .map((field) => buildListTile(field, context))
+                  .toList(),
             ),
           ),
         ],
@@ -80,154 +87,32 @@ class SettingsPage extends StatelessWidget {
     return Column(
       children: [
         ListTile(
-          title: Text(field.key),
-          subtitle: Text(field.value),
+          title: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  field.key, // First string on the left
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Text(
+                field.value, // Second string on the right
+                style: TextStyle(
+                  color: Color.fromRGBO(141, 136, 136, 0.75),
+                ),
+              ),
+            ],
+          ),
           trailing: Icon(Icons.arrow_forward),
           onTap: () {
             // Handle navigation or action
           },
         ),
-        Divider(height: 1, color: Colors.grey),
+        Padding(
+          padding: EdgeInsets.only(left: 18.0),
+          child: Divider(height: 1, color: Colors.grey),
+        ),
       ],
-    );
-  }
-}
-
-
-
-class EmailPage extends StatefulWidget {
-  @override
-  _EmailPageState createState() => _EmailPageState();
-}
-
-class _EmailPageState extends State<EmailPage> {
-  TextEditingController _emailController = TextEditingController();
-
-  void _saveEmail() {
-    // Handle email update logic here
-    String newEmail = _emailController.text;
-    // Update email logic goes here
-    print('New Email: $newEmail');
-    // You can add more logic here, such as showing a confirmation message.
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Email'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-          TextButton(
-            onPressed: _saveEmail,
-            child: Text(
-              'SAVE',
-              style: TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ],
-      ),
-      body: Container(
-        color: Colors.grey[200],
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Current Email: user@example.com'), // change to users.
-            SizedBox(height: 20),
-            Card(
-              elevation: 0,
-              child: ListTile(
-                title: TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: 'New Email',
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Option2Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Option 2'),
-      ),
-      body: Center(
-        child: Text('Content for Option 2'),
-      ),
-    );
-  }
-}
-
-class Option3Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Option 3'),
-      ),
-      body: Center(
-        child: Text('Content for Option 3'),
-      ),
-    );
-  }
-}
-
-class AppFeature1Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('App Feature 1'),
-      ),
-      body: Center(
-        child: Text('Content for App Feature 1'),
-      ),
-    );
-  }
-}
-
-class AppFeature2Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('App Feature 2'),
-      ),
-      body: Center(
-        child: Text('Content for App Feature 2'),
-      ),
-    );
-  }
-}
-
-class AppFeature3Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('App Feature 3'),
-      ),
-      body: Center(
-        child: Text('Content for App Feature 3'),
-      ),
     );
   }
 }
